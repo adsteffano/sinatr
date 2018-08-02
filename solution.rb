ef57630bae29f886ca4149d78a2ac2f30ec3a241
adsteffano <37783254+adsteffano@users.noreply.github.com>
2018-08-02 11:51:40 -0600

    el codigo secreto

--- solution.rb
+++ solution.rb
@@ -1,19 +1,6 @@
 require "sinatra"
 
-get '/' do
-	unless params[:nombre]
-		"Hola mundo"
-	else
-		"Hola #{params[:nombre]}"
-	end
-	erb :index
-end
-
-get '/:nombre' do
-	"Hola #{params[:nombre]}"
-end
-
-post '/nuevo' do
-	"!Hola #{params[:verbo]}!"	
+ get '/' do
+	"HcUy6Re2LLBRtj"
 end
 
